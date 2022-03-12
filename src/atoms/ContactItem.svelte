fd7caa9efdff3da3553ad93dd635318cc841ef2f
Filip Tomeš <filiptomes@protonmail.com>
2022-03-12 15:38:18 +0100

    contact page - desktop

---
+++ src/atoms/ContactItem.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let contactName, contactPos, contactMail
+</script>
+
+<div class="contact-item my-6 mx-4 text-center {$$props.class}">
+  <div class="mx-auto contact-desc">
+    <h6 class="text-pinky uppercase txt-bold mb-2 md:mb-4">{contactName}</h6>
+    <p class="manager-position text-black">{ contactPos }</p>
+    <a class="text-black txt-normal" href={`mailto:${contactMail}`}>{ contactMail }</a>
+  </div>
+</div>
+
+<style>
+
+@media screen and (min-width: 768px) {
+  .contact-item {
+    width: 40%;
+    margin: 1rem 2%;
+  } 
+}
+
+@media screen and (min-width: 1024px) {
+  .contact-item {
+    width: 24%;
+    margin: 1rem 2%;
+  }
+}
+
+.contact-desc {
+    width: fit-content;
+    min-width: 82%;
+  }
+  
+</style>
--- src/components/contact/ContactList.svelte
+++ src/components/contact/ContactList.svelte
@@ -6,7 +6,7 @@
       name: 'LEONA VYHNÁLKOVÁ',
       pos: 'manažerka projektu',
       mail: 'vyhnalkova@sutb.cz'
-    },
+    },    
     {
       name: 'Adam Utíkal',
       pos: 'statutární zástupce',
@@ -16,6 +16,16 @@
       name: 'Rastislav Škojec',
       pos: 'koordinátor spolupráce',
       mail: 'skojec@sutb.cz'
+    },    
+    {
+      name: 'martin šálek',
+      pos: 'vedoucí produkce',
+      mail: 'martin.salek@sutb.cz'
+    },    
+    {
+      name: 'adéla pokorná',
+      pos: 'koordinátorka účinkujících',
+      mail: 'pokorna@sutb.cz'
     },
     {
       name: 'MICHAELA FILIPCOVÁ',
@@ -25,8 +35,21 @@
   ]
 </script>
 
-<section id="contact-list">
-  {#each contacts as contact}
-    <ContactItem />
-  {/each}
+<section id="contact-list" class="py-12 md:py-24">
+  <div class="md:w-4/5 mx-auto px-8 md:px-0 flex justify-center md:justify-between items-center flex-wrap">
+    {#each contacts as contact}
+      <ContactItem contactName={contact.name} contactPos={contact.pos} contactMail={contact.mail} class="md:mx-4" />
+    {/each}
+  </div>
+  <hr class="w-1/5 mx-auto my-12 bg-dark_blu_txt" />
+  <div class="info-contact text-center">
+    <h6 class="text-pinky uppercase txt-bold mb-2 md:mb-4">infolinka</h6>
+    <a class="text-black txt-normal" href="mailto:su@utb.cz">su@utb.cz</a>
+  </div>
 </section>
+
+<style>
+  hr {
+    height: 2px;
+  }
+</style>
